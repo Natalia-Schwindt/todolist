4a77aaa7cc4bdab01028cd2501d7d4e20662c102
Natalia-Schwindt <naty8014@gmail.com>
2024-11-30 19:49:25 -0300

    mejorando los estilos

--- src/components/FilterTask.jsx
+++ src/components/FilterTask.jsx
@@ -2,7 +2,7 @@ import { Select } from "@chakra-ui/react";
 
 const FilterTask = () => {
   return (
-    <Select placeholder="Seleccione una opción">
+    <Select w="100%" placeholder="Seleccione una opción">
       <option value="todas">Todas</option>
       <option value="completas">Completas</option>
       <option value="incompletas">Incompletas</option>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,23 @@
-import { Box, VStack, Text, Heading } from "@chakra-ui/react";
+import { Box, VStack, HStack, Text, Heading } from "@chakra-ui/react";
 import InputTask from "./InputTask";
 import FilterTask from "./FilterTask";
 
 const TodoList = () => {
   return (
-    <VStack bg='beige' w='100%' h='100%'>
-      <Heading color="teal">Todo List</Heading>
-      <InputTask />
-      <FilterTask />
-      <Box bg="gray.100" p={4} borderRadius="md" shadow="md">
+    <VStack minH="100vh" w="100%" spacing={12} p={4}>
+      <Heading as="h1" fontSize="6xl" color="teal" textAlign="center">Todo List</Heading>
+      <HStack spacing={4} w="60%" justifyContent="center">
+        <InputTask />
+        <FilterTask />
+      </HStack>
+      <Box
+        bg="gray.100"
+        p={4}
+        borderRadius="md"
+        shadow="md"
+        w="100%"
+        maxW="600px"
+      >
         <Text fontSize="2xl" fontWeight="bold" mb={4} color="teal">
           Lista de Tareas
         </Text>
